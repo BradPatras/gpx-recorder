2e9c795120993cb2e2f0a990afbc8464241bad83
Brad Patras <bradpatras@brad-air.local>
2017-11-24 12:57:37 -0600

    Revert "adding permission handling singleton"
    This reverts commit 2cea3ae4656b19c329941d8065dce271d3a973f7.

--- app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
+++ app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.iboism.gpxrecorder.activity
 
-import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.NavigationView
@@ -9,27 +8,15 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.iboism.gpxrecorder.R
 import com.iboism.gpxrecorder.model.*
 import com.iboism.gpxrecorder.util.FileHelper
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.DexterBuilder
 import io.realm.RealmList
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
-import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
-import com.karumi.dexter.listener.single.PermissionListener
-import android.support.design.widget.Snackbar
-import android.view.View
-import com.iboism.gpxrecorder.util.PermissionHelper
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
 
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, PermissionListener {
-    val permissionHelper: PermissionHelper by lazy { PermissionHelper.getInstance(this) }
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,9 +24,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            permissionHelper.checkLocationPermissions(
-                    onAllowed = { startActivity(Intent(applicationContext, RecordingConfigurationActivity::class.java)) },
-                    onDenied = { /* do nothing for now*/  })
+            startActivity(Intent(applicationContext, RecordingConfigurationActivity::class.java))
         }
 
         val toggle = ActionBarDrawerToggle(
@@ -47,16 +32,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // Begin tests
+        var xmlMapper = XmlMapper()
+
         val segment = Segment(points = RealmList(TrackPoint(lat = 59.4408327f, lon = 24.74516185f), TrackPoint(lat = 59.4408330f, lon = 24.74516179f)))
         val track = Track(name = "test track", segments = RealmList(segment))
         val gpxContent = GpxContent(trackList = RealmList(track), title = "are you still there")
 
         val fileHelper = FileHelper(applicationContext)
+
+
+
         val file = fileHelper.gpxFileWith(gpxContent)
         fileHelper.shareFile(file)
-
-        // End tests
 
         nav_view.setNavigationItemSelectedListener(this)
     }
@@ -110,17 +97,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
-    }
-
-    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-
-    }
-
-    override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?, token: PermissionToken?) {
-        token?.continuePermissionRequest()
-    }
-
-    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
