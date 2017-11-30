d84544c1dccaa18f50df370e52411d00da691724
Brad <brad.iboism@gmail.com>
2017-11-29 23:40:09 -0600

    added possible app icons

--- app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
+++ app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
@@ -83,18 +83,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
             R.id.nav_share -> {
 
             }
