835ba5a02a100e04ef0b701e6f93ab85a45b2a91
Brad Patras <BradPatras@users.noreply.github.com>
2017-12-04 23:44:20 -0600

    think i'm gonna go iwth a fragment for the recording configuration screen. and wow i have been using teh wrong email all this time for commits rip

---
+++ app/src/main/java/com/iboism/gpxrecorder/fragment/MyRecordingConfigRecyclerViewAdapter.kt
@@ -0,0 +1,53 @@
+package com.iboism.gpxrecorder.fragment
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.iboism.gpxrecorder.fragment.RecordingConfigFragment.OnListFragmentInteractionListener
+import com.iboism.gpxrecorder.fragment.dummy.DummyContent.DummyItem
+
+/**
+ * [RecyclerView.Adapter] that can display a [DummyItem] and makes a call to the
+ * specified [OnListFragmentInteractionListener].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyRecordingConfigRecyclerViewAdapter(private val mValues: List<DummyItem>, private val mListener: OnListFragmentInteractionListener?) : RecyclerView.Adapter<MyRecordingConfigRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_recordingconfig, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.mItem = mValues[position]
+        holder.mIdView.text = mValues[position].id
+        holder.mContentView.text = mValues[position].content
+
+        holder.mView.setOnClickListener {
+            mListener?.onListFragmentInteraction(holder.mItem)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mValues.size
+    }
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mIdView: TextView
+        val mContentView: TextView
+        var mItem: DummyItem? = null
+
+        init {
+            mIdView = mView.findViewById(R.id.id) as TextView
+            mContentView = mView.findViewById(R.id.content) as TextView
+        }
+
+        override fun toString(): String {
+            return super.toString() + " '" + mContentView.text + "'"
+        }
+    }
+}
---
+++ app/src/main/java/com/iboism/gpxrecorder/fragment/RecordingConfigFragment.kt
@@ -0,0 +1,101 @@
+package com.iboism.gpxrecorder.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.iboism.gpxrecorder.R
+import com.iboism.gpxrecorder.fragment.dummy.DummyContent
+import com.iboism.gpxrecorder.fragment.dummy.DummyContent.DummyItem
+
+/**
+ * A fragment representing a list of Items.
+ *
+ *
+ * Activities containing this fragment MUST implement the [OnListFragmentInteractionListener]
+ * interface.
+ */
+/**
+ * Mandatory empty constructor for the fragment manager to instantiate the
+ * fragment (e.g. upon screen orientation changes).
+ */
+class RecordingConfigFragment : Fragment() {
+    // TODO: Customize parameters
+    private var mColumnCount = 1
+    private var mListener: OnListFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (arguments != null) {
+            mColumnCount = arguments.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_recordingconfig_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            val context = view.getContext()
+            if (mColumnCount <= 1) {
+                view.layoutManager = LinearLayoutManager(context)
+            } else {
+                view.layoutManager = GridLayoutManager(context, mColumnCount)
+            }
+            view.adapter = MyRecordingConfigRecyclerViewAdapter(DummyContent.ITEMS, mListener)
+        }
+        return view
+    }
+
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            mListener = context
+        } else {
+            throw RuntimeException(context!!.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem)
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        private val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        fun newInstance(columnCount: Int): RecordingConfigFragment {
+            val fragment = RecordingConfigFragment()
+            val args = Bundle()
+            args.putInt(ARG_COLUMN_COUNT, columnCount)
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
