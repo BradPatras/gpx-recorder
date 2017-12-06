e0695718fb8b78c9b16ecf8bab174cbb8d5a2a37
Brad <BradPatras@users.noreply.github.com>
2017-12-06 00:21:16 -0600

    creating recording configurator dialog

--- app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
+++ app/src/main/java/com/iboism/gpxrecorder/activity/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 onAllowed = {
                     // open recording creator
                     startRecording()
+
                 })
         }
 
---
+++ app/src/main/java/com/iboism/gpxrecorder/fragment/RecordingConfiguratorDialog.kt
@@ -0,0 +1,14 @@
+package com.iboism.gpxrecorder.fragment
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
+
+/**
+ * Created by Brad on 12/5/2017.
+ */
+class RecordingConfiguratorDialog: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
