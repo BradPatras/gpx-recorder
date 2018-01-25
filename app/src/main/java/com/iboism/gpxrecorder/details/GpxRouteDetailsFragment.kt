884c7832704c60dc124ee19770c8dfc6efaa04d3
Brad <BradPatras@users.noreply.github.com>
2018-01-24 23:24:13 -0600

    adding detail page for routes, havent actually deployed to device in a few commits

--- app/src/main/java/com/iboism/gpxrecorder/details/GpxRouteDetailsFragment.kt
+++ app/src/main/java/com/iboism/gpxrecorder/details/GpxRouteDetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.iboism.gpxrecorder.editing
+package com.iboism.gpxrecorder.details
 
 import android.content.Context
 import android.os.Bundle
@@ -8,23 +8,39 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.iboism.gpxrecorder.R
+import com.iboism.gpxrecorder.model.GpxContent
 import com.iboism.gpxrecorder.util.Keys
+import io.realm.Realm
+import kotlinx.android.synthetic.main.fragment_edit_gpxroute.*
 
-class EditGpxRouteFragment : Fragment() {
+class GpxRouteDetailsFragment : Fragment() {
 
     private var mListener: OnGpxEditedListener? = null
     private var gpxId: Long? = null
+    private var gpxContent: GpxContent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (arguments != null) {
             gpxId = arguments.getLong(Keys.GpxId)
         }
+
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_edit_gpxroute, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_edit_gpxroute, container, false)
+
+        Realm.getDefaultInstance().where(GpxContent::class.java)
+                .equalTo(GpxContent.primaryKey, gpxId)
+                .findFirst()?.let {
+            waypoint_listView.adapter = WaypointAdapter(gpxContent?.waypointList?.where()?.findAll())
+            title_editText.setText(it.title)
+        }
+
+
+        return rootView
     }
 
     override fun onAttach(context: Context?) {
@@ -48,10 +64,10 @@ class EditGpxRouteFragment : Fragment() {
     companion object {
         /**
          * @param gpxId gpxId of route to be edited.
-         * @return A new instance of fragment EditGpxRouteFragment.
+         * @return A new instance of fragment GpxRouteDetailsFragment.
          */
-        fun newInstance(gpxId: Long): EditGpxRouteFragment {
-            val fragment = EditGpxRouteFragment()
+        fun newInstance(gpxId: Long): GpxRouteDetailsFragment {
+            val fragment = GpxRouteDetailsFragment()
             val args = Bundle()
             args.putLong(Keys.GpxId, gpxId)
             fragment.arguments = args
