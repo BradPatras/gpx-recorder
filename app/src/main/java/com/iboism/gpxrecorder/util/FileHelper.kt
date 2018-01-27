7cfbc2440f47fd63ce02c77a893503fb1f212c0c
Brad <BradPatras@users.noreply.github.com>
2018-01-26 23:11:03 -0600

    broke up more blocks

--- app/src/main/java/com/iboism/gpxrecorder/util/FileHelper.kt
+++ app/src/main/java/com/iboism/gpxrecorder/util/FileHelper.kt
@@ -17,7 +17,6 @@ import java.nio.charset.StandardCharsets
  */
 
 private const val SHARE_PATH = "shared"
-private const val SHARE_FILENAME = "recorded"
 private const val REPLACE_CONTENT_TAG = "replacemewithcontent"
 private const val FILE_EXTENSION = ".gpx"
 
@@ -30,24 +29,26 @@ class FileHelper(var context: Context) {
         exporting = gpxContentId
         return Single.just(gpxContentId)
                 .observeOn(Schedulers.io())
-                .map {
-                    GpxContent.withId(Realm.getDefaultInstance(), it)
-                }.map {
-                    val sharedFilesPath = File(context.cacheDir, SHARE_PATH)
-                    sharedFilesPath.mkdirs()
+                .map { GpxContent.withId(Realm.getDefaultInstance(), it) }
+                .map { writeGpxToFile(it) }
+                .doFinally { exporting = null }
+    }
 
-                    val inputStream = context.resources.openRawResource(R.raw.gpx_stub)
-                    val gpxStub = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-                    inputStream.close()
+    private fun getGpxStub(): String {
+        val inputStream = context.resources.openRawResource(R.raw.gpx_stub)
+        val gpxStub = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+        inputStream.close()
+        return gpxStub
+    }
+    
+    private fun writeGpxToFile(gpx: GpxContent): File {
+        val sharedFilesPath = File(context.cacheDir, SHARE_PATH).apply { this.mkdirs() }
+        val gpxFull = getGpxStub().replaceFirst(REPLACE_CONTENT_TAG, gpx.getXmlString())
+        val gpxSharedFile = File(sharedFilesPath, gpx.title.getLegalFilename().withGpxExt())
 
-                    val gpxFull = gpxStub.replaceFirst(REPLACE_CONTENT_TAG, it.getXmlString())
-                    val fileName = it.title.getLegalFilename().withGpxExt()
-                    val gpxSharedFile = File(sharedFilesPath, fileName)
-                    FileUtils.writeStringToFile(gpxSharedFile, gpxFull, StandardCharsets.UTF_8)
-                    exporting = null
-                    gpxSharedFile
+        FileUtils.writeStringToFile(gpxSharedFile, gpxFull, StandardCharsets.UTF_8)
 
-                }.doOnError { exporting = null }
+        return gpxSharedFile
     }
 
     private fun String.getLegalFilename(): String {
