3d872018f81a01cd681ab60e0c15768b35403c89
Brad <BradPatras@users.noreply.github.com>
2018-05-08 00:58:04 -0500

    split configurator view from fragment class

--- app/src/main/java/com/iboism/gpxrecorder/recording/RecordingConfiguratorModal.kt
+++ app/src/main/java/com/iboism/gpxrecorder/recording/RecordingConfiguratorModal.kt
@@ -2,21 +2,15 @@ package com.iboism.gpxrecorder.recording
 
 import android.app.Dialog
 import android.content.Context
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.BottomSheetDialogFragment
-import android.support.v4.content.ContextCompat
+import android.support.design.widget.CoordinatorLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.SeekBar
-import android.widget.TextView
 import com.iboism.gpxrecorder.R
 import com.iboism.gpxrecorder.model.RecordingConfiguration
-import android.support.design.widget.CoordinatorLayout
 
 
 private const val INTERVAL_MIN = 1
@@ -24,20 +18,23 @@ private const val DISPLACEMENT_MIN = 2
 
 class RecordingConfiguratorModal : BottomSheetDialogFragment() {
 
-    var intervalSlider: SeekBar? = null
-    var intervalValue: TextView? = null
-    var displacementSlider: SeekBar? = null
-    var displacementValue: TextView? = null
-    var titleEditText: EditText? = null
-    var doneButton: Button? = null
-
+    lateinit var configuratorView: RecordingConfiguratorView
     var listener: Listener? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.config_dialog, container)?.apply {
-            initializeViews(this)
+            configuratorView = RecordingConfiguratorView(this)
+            configuratorView.doneButton.setOnClickListener {
+                listener?.configurationCreated (
+                        RecordingConfiguration(
+                                title = configuratorView.titleEditText.text.toString().takeIf { it.isNotEmpty() } ?: "Untitled",
+                                interval = configuratorView.intervalSlider.progress.toLong().plus(INTERVAL_MIN).times(60 * 1000),
+                                minDisplacement = configuratorView.displacementSlider.progress.plus(DISPLACEMENT_MIN).toFloat()
+                        ))
+                this@RecordingConfiguratorModal.dismiss()
+            }
         }
     }
 
@@ -58,68 +55,6 @@ class RecordingConfiguratorModal : BottomSheetDialogFragment() {
         }
 
         return dialog
-    }
-
-    private fun initializeViews(root: View) {
-        intervalSlider = root.findViewById(R.id.interval_seekBar)
-        displacementSlider = root.findViewById(R.id.displacement_seekBar)
-        titleEditText = root.findViewById(R.id.config_title_editText)
-        intervalValue = root.findViewById(R.id.interval_value)
-        displacementValue = root.findViewById(R.id.displacement_value)
-        doneButton = root.findViewById(R.id.start_button)
-
-        displacementSlider?.let {
-            it.progressDrawable
-                    ?.setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
-            it.thumb
-                    ?.setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
-            it.max -= DISPLACEMENT_MIN
-        }
-
-        intervalSlider?.let {
-            it.progressDrawable
-                    ?.setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
-            it.thumb
-                    ?.setColorFilter(ContextCompat.getColor(activity, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
-            it.max -= INTERVAL_MIN
-        }
-
-        intervalValue?.text = intervalSlider?.progress.toString()
-        displacementValue?.text = displacementSlider?.progress.toString()
-
-        intervalSlider?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-
-            override fun onProgressChanged(seekBar: SeekBar?, p1: Int, p2: Boolean) {
-                seekBar?.let {
-                    intervalValue?.text = (seekBar.progress + INTERVAL_MIN).toString()
-                }
-            }
-        })
-
-        displacementSlider?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-
-            override fun onProgressChanged(seekBar: SeekBar?, p1: Int, p2: Boolean) {
-                seekBar?.let {
-                    displacementValue?.text = (seekBar.progress + DISPLACEMENT_MIN).toString()
-                }
-            }
-        })
-
-        doneButton?.setOnClickListener {
-            listener?.configurationCreated (
-                    RecordingConfiguration(
-                            title = titleEditText?.text.toString().takeIf { it.isNotEmpty() } ?: "Untitled",
-                            interval = intervalSlider?.progress?.toLong()?.plus(INTERVAL_MIN)?.times(60 * 1000) ?: 900000,
-                            minDisplacement = displacementSlider?.progress?.plus(DISPLACEMENT_MIN)?.toFloat() ?: 5f
-                    ))
-            this.dismiss()
-        }
     }
 
     override fun onAttach(context: Context) {
---
+++ app/src/main/java/com/iboism/gpxrecorder/recording/RecordingConfiguratorView.kt
@@ -0,0 +1,49 @@
+package com.iboism.gpxrecorder.recording
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.EditText
+import com.iboism.gpxrecorder.R
+
+/**
+ * Created by brad on 5/8/18.
+ */
+
+private const val INTERVAL_MIN = 1
+private const val DISPLACEMENT_MIN = 2
+
+class RecordingConfiguratorView(root: View, val intervalSlider: SeekBar = root.findViewById(R.id.interval_seekBar), private val intervalValue: TextView = root.findViewById(R.id.interval_value), val displacementSlider: SeekBar = root.findViewById(R.id.displacement_seekBar), private val displacementValue: TextView = root.findViewById(R.id.displacement_value), val titleEditText: EditText = root.findViewById(R.id.config_title_editText), val doneButton: Button = root.findViewById(R.id.start_button)) {
+    private val context: Context = root.context
+
+    init {
+        val primaryColor = ContextCompat.getColor(context, R.color.colorPrimary)
+
+        intervalSlider.progressDrawable.setColorFilter(primaryColor, PorterDuff.Mode.SRC_IN)
+        intervalSlider.thumb.setColorFilter(primaryColor, PorterDuff.Mode.SRC_IN)
+        intervalSlider.max -= DISPLACEMENT_MIN
+
+        displacementSlider.progressDrawable.setColorFilter(primaryColor, PorterDuff.Mode.SRC_IN)
+        displacementSlider.thumb.setColorFilter(primaryColor, PorterDuff.Mode.SRC_IN)
+        displacementSlider.max -= DISPLACEMENT_MIN
+
+        intervalValue.text = intervalSlider.progress.toString()
+        displacementValue.text = displacementSlider.progress.toString()
+
+        intervalSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+            override fun onProgressChanged(seekBar: SeekBar?, p1: Int, p2: Boolean) {
+                seekBar?.let {
+                    intervalValue.text = (seekBar.progress + INTERVAL_MIN).toString()
+                }
+            }
+        })
+    }
+}
