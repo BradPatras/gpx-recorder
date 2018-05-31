67f5b7d7ad6045e3bace3f740a591c0fae1b3ad3
Brad <BradPatras@users.noreply.github.com>
2018-05-30 22:53:08 -0500

    added custom map markers

--- app/src/main/java/com/iboism/gpxrecorder/viewer/MapController.kt
+++ app/src/main/java/com/iboism/gpxrecorder/viewer/MapController.kt
@@ -6,20 +6,25 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.*
 import com.google.android.gms.maps.model.JointType.ROUND
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.iboism.gpxrecorder.R
 import com.iboism.gpxrecorder.model.GpxContent
 import com.iboism.gpxrecorder.model.Track
 import com.iboism.gpxrecorder.model.Waypoint
 import com.iboism.gpxrecorder.util.DateTimeFormatHelper
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.support.v4.content.res.ResourcesCompat
+import android.support.annotation.DrawableRes
+import com.google.android.gms.maps.model.BitmapDescriptor
 
 /**
  * Created by Brad on 2/26/2018.
  */
 
-class MapController(val context: Context, val gpxId: Long): OnMapReadyCallback {
+class MapController(private val context: Context, private val gpxId: Long): OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap?) {
         if (map == null) return // take no action if a map was not created
@@ -61,22 +66,41 @@ class MapController(val context: Context, val gpxId: Long): OnMapReadyCallback {
         tracks.firstOrNull()?.segments?.firstOrNull()?.points?.firstOrNull()?.let {
             this.addMarker(MarkerOptions().position(LatLng(it.lat, it.lon))
                     .title("Start")
-                    .snippet(DateTimeFormatHelper.toReadableString(it.time)))
+                    .snippet(DateTimeFormatHelper.toReadableString(it.time))
+                    .icon(getBitmapDescriptor(R.drawable.ic_start_pt))
+                    .anchor(.5f, .5f))
         }
         // draw marker at end
         tracks.lastOrNull()?.segments?.lastOrNull()?.points?.lastOrNull()?.let {
             this.addMarker(MarkerOptions().position(LatLng(it.lat, it.lon))
                     .title("End")
-                    .snippet(DateTimeFormatHelper.toReadableString(it.time)))
+                    .snippet(DateTimeFormatHelper.toReadableString(it.time))
+                    .icon(getBitmapDescriptor(R.drawable.ic_stop_pt))
+                    .anchor(.5f, .5f))
         }
     }
 
     private fun GoogleMap.drawWaypoints(waypoints: List<Waypoint>) {
         waypoints.forEach {
+            val dist = if (it.dist > 0.0) "@%.2fkm".format(it.dist) else ""
+            val snippet = if (it.desc.isBlank()) dist else "$dist: ${it.desc}"
             this.addMarker(MarkerOptions().position(LatLng(it.lat,it.lon))
                     .flat(false)
                     .title(it.title)
-                    .snippet(it.desc))
+                    .snippet(snippet)
+                    .icon(getBitmapDescriptor(R.drawable.ic_waypoint_pt))
+                    .anchor(.5f, .5f))
+
         }
+    }
+
+    private fun getBitmapDescriptor(@DrawableRes id: Int): BitmapDescriptor {
+        val vectorDrawable = ResourcesCompat.getDrawable(context.resources, id, null)
+        val bitmap = Bitmap.createBitmap(vectorDrawable!!.intrinsicWidth,
+                vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
