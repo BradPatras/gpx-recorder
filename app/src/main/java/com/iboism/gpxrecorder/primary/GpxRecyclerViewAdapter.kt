9a431e5b0b4a73277470d4d6880d6704d400cd7f
Brad <BradPatras@users.noreply.github.com>
2018-07-06 23:11:02 -0500

    moved touch listener to a new file

--- app/src/main/java/com/iboism/gpxrecorder/primary/GpxRecyclerViewAdapter.kt
+++ app/src/main/java/com/iboism/gpxrecorder/primary/GpxRecyclerViewAdapter.kt
@@ -99,8 +99,18 @@ class GpxRecyclerViewAdapter(contentList: OrderedRealmCollection<GpxContent>) : 
             contentViewerOpener?.invoke(gpx.identifier)
         }
 
-        viewHolder.rootView.setOnTouchListener(getSwipeHandler(position))
+        viewHolder.rootView.setOnTouchListener(GpxListSwipeHandler(position, { rowDismissed(position) }))
+    }
 
+    private fun rowDismissed(position: Int) {
+        Realm.getDefaultInstance().executeTransaction {
+            getItem(position)?.deleteFromRealm()
+            val mutableCache = cachedPoints.toMutableList()
+            mutableCache.removeAt(position)
+            cachedPoints = mutableCache.toTypedArray()
+        }
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, data?.size ?: 0)
     }
 
     inner class GpxViewHolder(view: View?): RecyclerView.ViewHolder(view) {
@@ -125,98 +135,6 @@ class GpxRecyclerViewAdapter(contentList: OrderedRealmCollection<GpxContent>) : 
 
             exportProgressBar.visibility = if (loading) View.VISIBLE else View.GONE
             exportProgressBar.invalidate()
-        }
-    }
-
-    private fun getSwipeHandler(position: Int): View.OnTouchListener {
-        return object: View.OnTouchListener {
-            private var startX = 0f
-            private var startY = 0f
-            private var deltaX = 0f
-            private var deltaY = 0f
-            private var currentTouch = false
-            private val position = position
-
-            override fun onTouch(v: View, event: MotionEvent): Boolean {
-                when(event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        startX = event.rawX
-                        startY = event.rawY
-                        return false
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        if (currentTouch && (deltaX.absoluteValue < 6 || deltaY.absoluteValue < 6)) {
-                            cancelSwipe(v)
-                            return false
-                        }
-
-                        if (currentTouch && deltaX.absoluteValue > v.width / 3) {
-                            v.animate()
-                                    .translationXBy(deltaX * 3f)
-                                    .withEndAction {
-                                        Realm.getDefaultInstance().executeTransaction {
-                                            getItem(position)?.deleteFromRealm()
-                                            val mutableCache = cachedPoints.toMutableList()
-                                            mutableCache.removeAt(position)
-                                            cachedPoints = mutableCache.toTypedArray()
-                                        }
-                                        notifyItemRemoved(this.position)
-                                        notifyItemRangeChanged(this.position, data?.size ?: 0)
-                                    }.start()
-
-                            return true
-                        }
-
-                        return cancelSwipe(v)
-                    }
-                    MotionEvent.ACTION_MOVE -> {
-                        deltaX = (event.rawX - startX)
-                        deltaY = (event.rawY - startY)
-                        val touchAngle = atan2(deltaY.absoluteValue, deltaX.absoluteValue) * (180 / PI)
-                        val horizontalSwipe = touchAngle in -1f..30f
-
-                        if (!currentTouch && horizontalSwipe) {
-                            currentTouch = true
-                            startX = event.rawX
-                            return true
-                        }
-
-                        if (currentTouch) {
-                            v.parent.requestDisallowInterceptTouchEvent(true)
-
-                            v.x = deltaX
-                            v.alpha = (v.width - (deltaX.absoluteValue * 1.3f)) / v.width
-                            return true
-                        }
-
-                        return false
-                    }
-                    MotionEvent.ACTION_CANCEL -> {
-                        return cancelSwipe(v)
-                    }
-                    MotionEvent.ACTION_SCROLL -> {
-                        return cancelSwipe(v)
-                    }
-                    else -> {
-                        return false
-                    }
-                }
-            }
-
-            private fun cancelSwipe(v: View): Boolean{
-                if (currentTouch) {
-                    v.parent.requestDisallowInterceptTouchEvent(false)
-                    currentTouch = false
-                    deltaX = 0f
-                    deltaY = 0f
-                    v.animate()
-                            .x(0f)
-                            .alpha(1f)
-                    return true
-                }
-
-                return false
-            }
         }
     }
 }
