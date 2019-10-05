4dbc1ca9d5fab96139d9a33eb0c40e2eb40520ca
Brad Patras <BradPatras@users.noreply.github.com>
2019-10-05 15:15:56 -0500

    fixed some translations, and verbiage.  updated current recording controller view to prep for showing the current route in a details context

--- app/src/main/java/com/iboism/gpxrecorder/recording/CurrentRecordingView.kt
+++ app/src/main/java/com/iboism/gpxrecorder/recording/CurrentRecordingView.kt
@@ -11,9 +11,9 @@ import kotlinx.android.synthetic.main.current_recording_view.view.*
 class CurrentRecordingView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
     private val rootView = LayoutInflater.from(context).inflate(R.layout.current_recording_view, this) as ConstraintLayout
     val routeTitle = route_title_tv
-    val addWaypointButton = add_wpt_view as Button
-    val playPauseButton = playpause_view as Button
-    val stopButton = stop_view as Button
+    val addWaypointButton = add_wpt_btn as Button
+    val playPauseButton = playpause_btn as Button
+    val stopButton = stop_btn as Button
 
     fun setPaused(isPaused: Boolean) {
         playPauseButton.text = if (isPaused) context.getText(R.string.resume_recording) else context.getText(R.string.pause_recording)
